f72524e4817e5011b9ff47526d99590db9470ebb
Matt Rogge <5900418+mrrogge@users.noreply.github.com>
2021-04-14 21:19:21 -0500

    .

--- rude/plugins/lovePlugin.lua
+++ rude/plugins/lovePlugin.lua
@@ -1,4 +1,34 @@
-return function(engine)
+--an asset loader for fonts
+local function fontAssetLoader(id)
+    local args = {}
+    local i, j = 1, string.find(id, ',', i)
+    while j do
+        table.insert(args, string.sub(id, i, j))
+        i, j = j+1, string.find(id, j+1)
+    end
+    table.insert(args, string.sub(id, i))
+    local fileName, size, hinting, dpiScale, imageFileName
+    if tonumber(args[1]) then
+        size, hinting, dpiScale = tonumber(args[1]), args[2], 
+            tonumber(args[3])
+    else
+        fileName = args[1]
+        if tonumber(args[2]) then
+            size, hinting, dpiScale = tonumber(args[2]), args[3],
+                tonumber(args[4])
+        else
+            imageFileName = args[2]
+        end
+    end
+    if fileName and imageFileName then
+        return love.graphics.newFont(fileName, imageFileName)
+    else
+        return love.graphics.newFont(fileName, size, hinting, dpiScale)
+    end
+end
+
+--Return the plugin as a function
+return function(engine, context)
     local missingLoveMsg = 'This plugin requires LÖVE framework v11.0 or newer to run correctly. See: https://love2d.org.'
     if not love then
         error(missingLoveMsg)
@@ -8,49 +38,20 @@ return function(engine)
         error(missingLoveMsg)
     end
 
-    --register love.graphics.newImage as an asset loader
-    engine:registerAssetLoader('image', love.graphics.newImage)
-
-    --register an asset loader for fonts
-    engine:registerAssetLoader('font', function(id)
-        local args = {}
-        local i, j = 1, string.find(id, ',', i)
-        while j do
-            table.insert(args, string.sub(id, i, j))
-            i, j = j+1, string.find(id, j+1)
-        end
-        table.insert(args, string.sub(id, i))
-        local fileName, size, hinting, dpiScale, imageFileName
-        if tonumber(args[1]) then
-            size, hinting, dpiScale = tonumber(args[1]), args[2], 
-                tonumber(args[3])
-        else
-            fileName = args[1]
-            if tonumber(args[2]) then
-                size, hinting, dpiScale = tonumber(args[2]), args[3],
-                    tonumber(args[4])
-            else
-                imageFileName = args[2]
-            end
-        end
-        if fileName and imageFileName then
-            return love.graphics.newFont(fileName, imageFileName)
-        else
-            return love.graphics.newFont(fileName, size, hinting, dpiScale)
-        end
-    end)
+    --register asset loaders
+    engine:registerAssetLoader('image', love.graphics.newImage, context)
+    engine:registerAssetLoader('font', fontAssetLoader, context)
 
     --add a table for LOVE-related functionality
     engine.love = engine.love or {
-        _initCallbacks={
-            _attached = false
-        }
+        _initCallbacks={},
+        _attached = false
     }
 
     ---Sets up the LOVE environment to use callback functions defined for this engine.
     -- Any modifications made to the LOVE callbacks prior to calling attach() will be preserved.
     engine.love.attach = function()
-        if _attached then return engine end
+        if engine.love._attached then return end
         engine.love._initCallbacks.load = love.load
         engine.love._initCallbacks.update = love.update
         engine.love._initCallbacks.draw = love.draw
@@ -66,18 +67,18 @@ return function(engine)
             end
             engine:load(...)
         end
-        love.update = function(dt)
+        love.update = function(dt, ...)
             c('rn')
             if engine.love._initCallbacks.update then
-                engine.love._initCallbacks.update(dt)
+                engine.love._initCallbacks.update(dt, ...)
             end
-            engine:update(dt)
+            engine:update(dt, ...)
         end
-        love.draw = function()
+        love.draw = function(...)
             if engine.love._initCallbacks.draw then
-                engine.love._initCallbacks.draw()
+                engine.love._initCallbacks.draw(...)
             end
-            engine:draw()
+            engine:draw(...)
         end
         love.keypressed = function(key, scancode, isrepeat)
             c('rs,rs,rb')
@@ -122,14 +123,13 @@ return function(engine)
             engine:wheelmoved(x,y)
         end
         engine.love._attached = true
-        return engine
     end
 
     ---Detaches the engine from the LOVE environment.
     -- This resets all LOVE callbacks to their original functions. Does nothing if the engine is not currently attached.
     engine.love.detach = function()
         c('rt')
-        if not engine.love._attached then return engine end
+        if not engine.love._attached then return end
         for k,v in pairs(engine.love._initCallbacks) do
             love[k] = v
             engine.love._initCallbacks[k] = nil
