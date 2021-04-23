e966fb8451c649201f167ea493226208b8d63f18
Matt Rogge <5900418+mrrogge@users.noreply.github.com>
2021-04-23 09:45:45 -0500

    add Exception class

---
+++ rude/Exception.lua
@@ -0,0 +1,21 @@
+---An exception object.
+-- Exceptions can be used to identify "exceptional" branches in code. They use debug.getinfo() to identify the calling source and line of code, which makes it easy to log the exception or raise it as an error. Note that Exceptions do not automatically create errors, they are just plain objects.
+
+local class = require('middleclass')
+
+local Exception = class('Exception')
+
+function Exception:initialize(msg)
+    self.msg = msg
+    local info = debug.getinfo(2, 'Sl') --get the source and current line of the calling function
+    if info then
+        if info.what == 'C' then
+            self.src, self.line = 'c func', '??'
+        else
+            self.src, self.line = info.short_src, info.currentline
+        end
+    end
+    return self    
+end
+
+return Exception
