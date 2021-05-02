e4fbcddb26c04e27ecd202cf5a0c6d7b28b55a8a
Matt Rogge <5900418+mrrogge@users.noreply.github.com>
2021-05-02 15:36:07 -0500

    re-add assert for matching type

--- spec/init.lua
+++ spec/init.lua
@@ -19,3 +19,11 @@ end
 say:set('assertion.instanceOf.positive', 'Expected object %s to be instance of %s')
 say:set('assertion.instanceOf.negative', 'Expected object %s to not be instance of %s')
 luassert:register('assertion', 'instanceOf', isInstanceOf, 'assertion.instanceOf.positive', 'assertion.instanceOf.negative')
+
+--assert that a value is the specified type.
+local function isType(state, args)
+    return type(args[1]) == args[2]
+end
+say:set('assertion.isType.positive', 'Expected %s to be type: %s')
+say:set('assertion.isType.negative', 'Expected %s to not be type: %s')
+luassert:register('assertion', 'Type', isType, 'assertion.isType.positive', 'assertion.isType.negative')
